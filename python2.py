58597ba712ffe402b696a04624b6c3920573a2c3
volodya52 <157564207+volodya52@users.noreply.github.com>
2024-12-18 19:24:54 +0500

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,34 @@
+import random
+
+#Задание 1
+#b=random.randint(0,10)
+#i=0
+#while True:
+ #   a=int(input ("Введите число от 0 до 10"))
+ #   if a<0:
+  #      print("Число не должно быть меньше 0")
+   # elif a>b:
+    #    print("Ваше число больше числа, загаданным компьютером")
+     #   i+=1
+   # elif a<b:
+    #    print("Ваше число меньше числа, загаданного компьютером")
+     #   i+=1
+  #  elif a==b:
+   #     print("Вы угадали число, которое загадал компьютер!!!")
+    #    break
+
+
+
+#Задание 2
+#inputword=str(input("Введите слово"))
+#razdel=inputword.split()
+#gruppirovka={}
+#for simvol in razdel:
+#    if simvol in gruppirovka:
+#        gruppirovka[simvol].append(simvol)
+#    else:
+#        gruppirovka[simvol]=[simvol]
+#        result=list(gruppirovka.values())      
+#print(result)
+
+
